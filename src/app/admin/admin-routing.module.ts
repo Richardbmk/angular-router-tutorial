24655a7c15b3ff83ac440b564825091a6a846bef
Richardbmk <rdobmk@gmail.com>
2023-11-06 21:11:56 +0100

    Guard the admin feature

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from '../auth/auth.guard';
 import { AdminComponent } from './admin/admin.component';
 import { ManageCrisesComponent } from './manage-crises/manage-crises.component';
 import { ManageHeroesComponent } from './manage-heroes/manage-heroes.component';
@@ -9,6 +10,8 @@ const routes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
+    canActivate: [authGuard],
+
     children: [
       {
         path: '',
