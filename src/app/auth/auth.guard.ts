4176ca161d8e90dd04b847380a078d7bc89f8df4
Richardbmk <rdobmk@gmail.com>
2023-11-06 21:27:00 +0100

    Authenticate with authGuard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,6 +1,18 @@
+import { Inject, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from './auth.service';
+
 import { CanActivateFn } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  console.log('authGuard#canActivate called');
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn) {
+    return true;
+  }
+
+  // Redirect to the login page
+  return router.parseUrl('/login');
 };
